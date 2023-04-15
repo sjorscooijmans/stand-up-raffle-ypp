72830b68828e728c7c58c0ce449dd744bb65dcdb
Sjors Cooijmans <sjors.cooijmans@hotmail.com>
2023-04-15 17:31:21 +0200

    updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +15,35 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [IonicModule],
+  animations: [
+    trigger('rotating', [
+      state('begin', style({
+      })),
+      state('end', style({
+        transform: "translateY(100vw)"
+      })),
+      transition('begin => end', [
+        animate('1s', keyframes([
+          style({ transform: 'translateY(100vw)' }),
+        ]))
+      ]),
+    ])
+  ]
 })
 export class HomePage {
-  constructor() {}
+  start = true
+
+  personen = [
+    {name: 'sjors', image: 'assets/personen/Leo.PNG'},
+    {name: 'leonard', image: 'assets/personen/Sjors.PNG'},
+    {name: 'martijn', image: 'assets/personen/Martijn.PNG'},
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    console.log('test')
+  }
 }
