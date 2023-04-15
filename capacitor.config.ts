678b93b8c30211adc97eaaa039936309e0b22480
Sjors Cooijmans <sjors.cooijmans@hotmail.com>
2023-04-15 14:30:53 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'stand-up-raffle-ypp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
